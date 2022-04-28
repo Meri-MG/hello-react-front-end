0b7b133d87e53a2124d0c6bd7bfdd79a08d45f1a
Meri <mgogi13@freeuni.edu.ge>
2022-04-28 14:55:56 +0400

    feat: setup components and config package.json

---
+++ src/redux/configureStore.js
@@ -0,0 +1,35 @@
+import { legacy_createStore as createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { GET_GREETINGS_SUCCESS } from "../components/HelloWorld";
+
+const inititalState = {
+  greetings: [
+    {
+      name: "Hi",
+    },
+  ],
+};
+
+
+function rootReducer(state, action) {
+  console.log(action.type);
+  switch (action.type) {
+    case GET_GREETINGS_SUCCESS:
+      return { greetings: action.json };
+    default:
+      return state;
+  }
+}
+
+
+export default function configureStore(){
+    const store = createStore(
+        rootReducer,
+        inititalState,
+        composeWithDevTools(
+          applyMiddleware(thunk)
+        )
+        );
+    return store
+}
